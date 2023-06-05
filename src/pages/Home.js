556adda7b549bd7ebe1b4b39eb233027e386dc75
Badreddine Mouhli <Badramouhli@hotmail.com>
2023-06-05 17:39:01 +0200

    Menu

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,10 @@ export default function Home() {
   return (
     <div className="home">
       <h1>My Progression</h1>
-      <div>Welcome to this site devoted to your programing progression</div>
+      <div>
+        Welcome to this site devoted to your programing progression
+        {new Date().toString()}
+      </div>
     </div>
   );
 }
